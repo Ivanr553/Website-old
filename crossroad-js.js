69ca5dae0f4fa7a082f744e912d1f2a1e0fd651b
Ivan Raffucci <ivanr553@gmail.com>
2017-03-11 00:57:07 -0800

    Create crossroad-js.js
    This is the new js for the crossroad html. Only added in functionality to open links at the moment.

---
+++ crossroad-js.js
@@ -0,0 +1,11 @@
+function openmain() {
+  window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/mainhtml.html", "_self")
+}
+
+function opengit() {
+  window.open("https://github.com/Ivanr553");
+}
+
+function openeducation() {
+  window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/education-html.html", "_self");
+}
