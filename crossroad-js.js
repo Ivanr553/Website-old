e8c61679d513d2ef658a3cfb7b4bab861b6a6b76
Ivan Raffucci <ivanr553@gmail.com>
2017-03-14 14:00:05 -0700

    Update-1-crossroad-js
    Added new functions to allow for each bubble to have a link to each page it is meant to direct to.

--- crossroad-js.js
+++ crossroad-js.js
@@ -1,11 +1,24 @@
-function openmain() {
-  window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/mainhtml.html", "_self")
+function openResume() {
+  //made changes here
+  window.open("https://docs.google.com/document/d/1womPl9LSMAqnITcXp-oxCOav6rPgmvj1imeujseu-JA/edit?usp=sharing");
 }
 
-function opengit() {
+function openGit() {
   window.open("https://github.com/Ivanr553");
 }
 
-function openeducation() {
+function openEducation() {
   window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/education-html.html", "_self");
+}
+
+function openBlog() {
+  window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/blog-html.html", "_self");
+}
+
+function openFccCert() {
+  window.open("https://www.freecodecamp.com/ivanr553/front-end-certification");
+}
+
+function openWork() {
+  window.open("file:///C:/Users/Ivan/Documents/WebDev/Website/work-html.html");
 }
