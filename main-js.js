35cde5ef200639192da024688c7b6c7baedde6ef
Ivan Raffucci <ivanr553@gmail.com>
2017-03-14 13:56:28 -0700

    Update and rename main-js.js to work-js.js
    Removed the previous js from main-js since I scrapped those items/ideas from my website. Will add in more js functionality when necessary.

--- main-js.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function animateheader() {
-  document.getElementById("header-h1").style.display = "none";}, 3000);
-setTimeout(function animateheader() {
-  document.getElementById("header-h1").style.display = "none";
-  document.getElementById("header-span1").style.display = "none";
-  document.getElementById("header-span2").style.display = "none";document.getElementById("name").style.display = "flex";
-}, 11000);
