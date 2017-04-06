4cdb1df27ede850d05e6f7b4f5ab9d7ad4eda93a
Ivan Raffucci <ivanr553@gmail.com>
2017-04-05 17:23:47 -0700

    Update work-js.js

--- javascript/work-js.js
+++ javascript/work-js.js
@@ -1,9 +1,11 @@
 function openCarla() {
   window.open("https://carlakunkelweb.github.io/website/");
 }
+
 function openMain() {
   window.open("index", "_self");
 }
+
 function openRaincheck() {
   window.open("https://frozen-springs-75857.herokuapp.com/");
 }
